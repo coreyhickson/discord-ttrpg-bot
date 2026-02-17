035bb7abb9f90226a1b557767833c6895e7f85b9
Corey Hickson <corey.r.hickson@gmail.com>
2026-02-16 19:43:30 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,6 +14,10 @@ class IdleHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"Bot is alive!")
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+
 def run_server():
     port = int(os.environ.get('PORT', 10000))
     server = HTTPServer(('0.0.0.0', port), IdleHandler)
